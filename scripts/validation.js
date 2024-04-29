d0b7e310bc1547592235f014e6f5b27271c8f5aa
RicardoPerdomo <ricardoperdomo0814@gmail.com>
2024-04-29 12:48:35 -0400

    Fixed

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,27 +1,26 @@
 function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
   const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
   inputEl.classList.add(inputErrorClass);
-  errorMessageEl.textcontent = inputEl.validationMessage;
+  errorMessageEl.textContent = inputEl.validationMessage;
   errorMessageEl.classList.add(errorClass);
 }
 
 function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
   const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
   inputEl.classList.remove(inputErrorClass);
-  errorMessageEl.textcontent = "";
+  errorMessageEl.textContent = "";
   errorMessageEl.classList.remove(errorClass);
 }
 
 function checkInputValidity(formEl, inputEl, options) {
   if (!inputEl.validity.valid) {
-    showInputError(formEl, inputEl, options);
-  } else {
-    hideInputError(formEl, inputEl, options);
+    return showInputError(formEl, inputEl, options);
   }
+  hideInputError(formEl, inputEl, options);
 }
 
 function hasInvalidInput(inputList) {
-  return inputList.every((inputEl) => inputEl.validity.valid);
+  return !inputList.every((inputEl) => inputEl.validity.valid);
 }
 
 function enableButton(submitBtn, { inactiveButtonClass }) {
@@ -29,14 +28,14 @@ function enableButton(submitBtn, { inactiveButtonClass }) {
   submitBtn.disabled = false;
 }
 
-function disabledButton(submitBtn, { inactiveButtonClass }) {
+function disableButton(submitBtn, { inactiveButtonClass }) {
   submitBtn.classList.add(inactiveButtonClass);
   submitBtn.disabled = true;
 }
 
 function toggleButtonState(inputEls, submitBtn, options) {
   if (hasInvalidInput(inputEls)) {
-    disabledButton(submitBtn, options);
+    disableButton(submitBtn, options);
     return;
   }
   enableButton(submitBtn, options);
@@ -64,13 +63,13 @@ function enableValidation(options) {
   });
 }
 
-const options = {
+const config = {
   formSelector: ".modal__form",
-  inputSelector: ".modal__form_input",
+  inputSelector: ".modal__form-input",
   submitButtonSelector: ".modal__save-button",
   inactiveButtonClass: "modal__save-button-disabled",
   inputErrorClass: "modal__form_input_type_error",
   errorClass: "modal__form-input-error-visible",
 };
 
-enableValidation(options);
+enableValidation(config);
